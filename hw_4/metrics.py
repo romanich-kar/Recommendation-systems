41d3f536888f17d733a9e540512a6611bade19cb
romanich-kar <81390143+romanich-kar@users.noreply.github.com>
2022-08-25 23:24:53 +0300

    Create metrics.py
    Модуль с часто используемыми метриками для оценки рекомендательных систем.

---
+++ hw_4/metrics.py
@@ -0,0 +1,147 @@
+﻿import numpy as np
+
+def precision(recommended_list, bought_list):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    precision = flags.sum() / len(recommended_list)
+    
+    return precision
+
+
+def precision_at_k(recommended_list, bought_list, k=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    bought_list = bought_list  # Тут нет [:k] !!
+    recommended_list = recommended_list[:k]
+    
+    flags = np.isin(bought_list, recommended_list)
+    precision = flags.sum() / len(recommended_list)
+    
+    
+    return precision
+
+
+def money_precision_at_k(recommended_list, bought_list, prices_recommended, k=5):
+        
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)[:k]
+    prices_recommended_list = np.array(prices) [:k]
+    
+    bought_list = bought_list  # Тут нет [:k] !!
+    recommended_list = recommended_list[:k]
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    precision = np.sum(flags*prices_recommended_list) / np.sum(prices_recommended_list)
+    
+    return precision
+
+
+def hit_rate(recommended_list, bought_list):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    hit_rate = (flags.sum() > 0).astype(int)
+    
+    return hit_rate
+
+
+def hit_rate_at_k(recommended_list, bought_list, k=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)[:k]
+    flags = np.isin(bought_list, recommended_list)
+    
+    hit_rate = (flags.sum() > 0).astype(int)
+    
+    return hit_rate
+
+
+
+
+def recall(recommended_list, bought_list):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    recall = flags.sum() / len(bought_list)
+    
+    return recall
+
+
+def recall_at_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)[:k]
+    
+    flags = np.isin(bought_list, recommended_list)
+    
+    recall = flags.sum() / len(bought_list)
+    
+    return recall
+
+
+def ap_k(recommended_list, bought_list, k=5):
+    
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    
+    flags = np.isin(recommended_list, bought_list)
+    
+    if sum(flags) == 0:
+        return 0
+    
+    sum_ = 0
+    for i in range(1, k+1): 
+        if flags[i] == True:
+            p_k = precision_at_k(recommended_list, bought_list, k=i)
+            sum_ += p_k
+            
+    result = sum_ / sum(flags)
+    
+    return result
+
+def reciprocal_rank_at_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    recommended_list = recommended_list[:k]
+    rank = 1
+    for i in range(len(recommended_list)):
+        if np.isin(recommended_list[i], bought_list):
+            rank = i+1
+            break
+        else:
+            continue  
+    rr_at_k = 1/rank
+
+    return rr_at_k
+  
+ def ndcg_at_k(recommended_list, bought_list, k=5):
+    bought_list = np.array(bought_list)
+    recommended_list = np.array(recommended_list)
+    recommended_list = recommended_list[:k]
+    dcg_at_k = 0
+    flags = np.isin(recommended_list,bought_list)
+    for i in range(len(flags)):
+        dcg_at_k += flags[i]/math.log2(i+2)
+   
+    idcg_at_k = 0
+    for i in range(len(flags)):
+        if flags[i]:
+            idcg_at_k = 1/math.log2(i+2)
+            break
+        else:
+            continue
+        
+    ndcg_at_k = dcg_at_k/idcg_at_k
+    
+    return ndcg_at_k
